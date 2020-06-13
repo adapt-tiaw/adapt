7489dcd686e9a0c00d0fe5297add336dfd343bd7
davigdc <davigdc10@gmail.com>
2020-06-13 16:28:03 -0300

    feature localstorage

--- login.js
+++ login.js
@@ -1,19 +1,99 @@
-function toggleResetPswd(e){
-    e.preventDefault();
-    $('#logreg-forms .form-signin').toggle() // display:block or none
-    $('#logreg-forms .form-reset').toggle() // display:block or none
+// Configura os botões
+document.getElementById('btnLogin').addEventListener('click', login);
+document.getElementById('btnSignIn').addEventListener('click', includeUser);
+
+//Busca se há dados ja cadastrados no local storage, se não retorna falso
+function readLocalStorage() {
+  let stringData = localStorage.getItem('db');
+  let newLocalStorage = {
+    session: {
+      loged: false,
+      email: null,
+    },
+    users: [],
+  };
+
+  if (stringData) {
+    return JSON.parse(stringData);
+  } else {
+    saveOnLocalStorage(newLocalStorage);
+    return JSON.parse(localStorage.getItem('db'));
+  }
 }
 
-function toggleSignUp(e){
-    e.preventDefault();
-    $('#logreg-forms .form-signin').toggle(); // display:block or none
-    $('#logreg-forms .form-signup').toggle(); // display:block or none
+//Salva dados no local storage
+function saveOnLocalStorage(data) {
+  localStorage.setItem('db', JSON.stringify(data));
 }
 
-$(()=>{
-    // Login Register Form
-    $('#logreg-forms #forgot_pswd').click(toggleResetPswd);
-    $('#logreg-forms #cancel_reset').click(toggleResetPswd);
-    $('#logreg-forms #btn-signup').click(toggleSignUp);
-    $('#logreg-forms #cancel_signup').click(toggleSignUp);
-})
+// Incluir um novo usuário
+function includeUser() {
+  //Valor dos inputs no form de cadastro
+  let strNameSignIn = document.getElementById('name_cad').value;
+  let strEmailSignIn = document.getElementById('email_cad').value;
+  let strPasswordSignIn = document.getElementById('password_cad').value;
+
+  if (strNameSignIn === '' || strEmailSignIn === '' || strPasswordSignIn === '')
+    return alert('Preencha todos os campos!');
+
+  // Recupera dados do Local Storage
+  let db = readLocalStorage();
+
+  // Verifica se email existe, caso sim retorna seu index, se não retorna -1
+  let user = db.users.findIndex(user => user.email === strEmailSignIn);
+
+  console.log(user);
+
+  if (user !== -1) return alert('Email já cadastrado!');
+
+  // Cria novo usuário
+  let newUser = {
+    name: strNameSignIn,
+    email: strEmailSignIn,
+    password: strPasswordSignIn,
+  };
+
+  // Adiciona novo usuário a base de usuários
+  db.users.push(newUser);
+
+  // Define usuário logado
+  db.session.loged = true;
+  db.session.email = newUser.email;
+
+  // Salva os dados no Local Storage
+  saveOnLocalStorage(db);
+
+  window.location.href = 'perfis.html';
+}
+
+function login() {
+  //Valor dos inputs no form de login
+  let strEmailLogin = document.getElementById('email').value;
+  let strPasswordLogin = document.getElementById('password').value;
+
+  if (strEmailLogin === '' || strPasswordLogin === '')
+    return alert('Preencha os campos!');
+
+  // Recupera dados do Local Storage
+  let db = readLocalStorage();
+
+  // Verifica se email existe, caso sim retorna objeto do usuário, se não retorna undefined
+  let user = db.users.find(user => user.email === strEmailLogin);
+
+  if (user) {
+    // Verifica se a senha está correta
+    if (user.password !== strPasswordLogin) {
+      alert('Senha incorreta');
+    } else {
+      // Define usuário logado e redireciona para página inicial
+      db.session.loged = true;
+      db.session.email = user.email;
+
+      saveOnLocalStorage(db);
+
+      window.location.href = 'adapt.html';
+    }
+  } else {
+    alert('E-mail não cadastrado, cadastre primeiro');
+  }
+}

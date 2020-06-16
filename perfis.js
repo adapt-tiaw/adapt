042f0a01928f9a53146c75171a62dc3c4672a356
davigdc <davigdc10@gmail.com>
2020-06-15 22:22:26 -0300

    Adicionado perfil ao usuario

---
+++ perfis.js
@@ -0,0 +1,32 @@
+// Busca dados cadastrados no local storage
+function readLocalStorage() {
+  let stringData = localStorage.getItem('db');
+
+  if (stringData) {
+    return JSON.parse(stringData);
+  } else {
+    alert('Usuario não autenticado!');
+    window.location.href = 'adapt.html';
+  }
+}
+
+function saveOnLocalStorage(data) {
+  localStorage.setItem('db', JSON.stringify(data));
+}
+
+function profile(prop) {
+  let db = readLocalStorage();
+
+  let userEmail = db.session.email;
+
+  let userIndex = db.users.findIndex(user => user.email === userEmail);
+  let user = db.users.find(user => user.email === userEmail);
+
+  user.profile = prop;
+
+  db.users[userIndex] = user;
+
+  saveOnLocalStorage(db);
+
+  window.location.href = `rotina${prop}.html`;
+}
